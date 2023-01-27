48fc1c58787a1e0240ec5db9fdd1a11d4c5dd7f1
Agata Wyciślik <agata.wycislik@gmail.com>
2023-01-27 21:12:40 +0100

    Add env usage

---
+++ cypress/po/products.ts
@@ -0,0 +1,8 @@
+export class Products {
+  getShoppingCart() {
+    return cy.get("#shopping_cart_container");
+  }
+  addProduct() {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  }
+}

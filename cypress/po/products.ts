cd481cf659d69f033bec18f4f0a62cfd8f29f5ba
Agata Wyciślik <agata.wycislik@gmail.com>
2023-01-29 18:29:16 +0100

    Remove unused methods

--- cypress/po/products.ts
+++ cypress/po/products.ts
@@ -2,6 +2,7 @@ export class Products {
   getShoppingCart() {
     return cy.get("#shopping_cart_container");
   }
+
   addProduct() {
     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
   }
